385eebb262fa6803b57a4bf91a4d375297fa3e97
M Faisal Basri <90618305+MFaisalBasri@users.noreply.github.com>
2023-05-11 11:31:10 +0700

    Update cek_login2.php
    menambahkan error handler

--- system/models/login/cek_login2.php
+++ system/models/login/cek_login2.php
@@ -1,5 +1,7 @@
 
 <?php
+error_reporting(0);
+ini_set('display_errors', 0);
 include '../../config/koneksi.php';
 		if(isset($_POST['login'])){
 			$user = mysqli_real_escape_string($conn, $_POST['user']);
